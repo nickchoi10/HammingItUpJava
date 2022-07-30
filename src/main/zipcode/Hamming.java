64f5a29a78f30a509c7281cba5cb639173c84924
Nick Choi <96934316+nickchoi10@users.noreply.github.com>
2022-07-30 18:21:54 -0400

    completed

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,24 @@
 package zipcode;
 
 public class Hamming {
-    public Hamming(String s, String s1) {
+    private String s1;
+    private String s;
+
+    public Hamming(String s, String s1) throws IllegalArgumentException {
+        this.s = s;
+        this.s1 = s1;
+        if (s.length() != s1.length()) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
     }
 
     public int getHammingDistance() {
-        return -1;
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != s1.charAt(i)) {
+                count++;
+            }
+        }
+        return count;
     }
 }
